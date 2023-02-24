3bc0249cd33c6f69c8f3b095e37cca3e81eb54c2
Sami <s.cyber.sami@gmail.com>
2023-02-24 15:29:48 +0400

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+//1
+
+//Process pr1 = new Process();
+//pr1.StartInfo.FileName = "notepad.exe";
+//pr1.Start();
+//pr1.WaitForExit();
+//Console.WriteLine("finish");
+
+//2
+//Process pr2 = new Process();
+//pr2.StartInfo.FileName = "notepad.exe";
+//pr2.Start();
+//Console.WriteLine("подождать до конца - 1\nвыключить принудительно - 2");
+//int a = int.Parse(Console.ReadLine());
+//if (a == 1)
+//{
+//    pr2.WaitForExit();
+//}
+//if (a == 2)
+//{
+//    pr2.Kill();
+//}
+//Console.WriteLine("finish");
